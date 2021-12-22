bb4e08468913bbb66047339888e647d5d0f05a6e
CJ Karkut <cjkarkut@gmail.com>
2021-12-21 22:34:38 -0600

    day 021

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var catNames = catData.filter(function (names) { return typeof names === "string
 var catAges = catData.filter(function (ages) { return typeof ages === 'number'; });
 console.log("Names: ", catNames);
 console.log("Ages: ", catAges);
-document.body.innerHTML = "\n<<<<<<< HEAD\n    <ul>\n      ".concat(catNames
+document.body.innerHTML = "\n    <ul>\n      ".concat(catNames
     .map(function (name, index) { return "<li>".concat(name, " - ").concat(catAges[index], "</li>"); })
     .join(""), "\n    </ul>\n  ");
 console.log();
--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 const catData: (number | string)[] = [
-<<<<<<< HEAD
   "Jeffery",
   3,
   "Mittens",
@@ -16,31 +15,12 @@ const catData: (number | string)[] = [
 
 const catNames: string[] = catData.filter(names => typeof names === "string");
 const catAges: number[] = catData.filter(ages => typeof ages === 'number');
-=======
-    "Jeffery",
-    3,
-    "Mittens",
-    4,
-    "Scooter",
-    2,
-    "Wilson",
-    1,
-    "Jeremy",
-    11,
-    "Alphonse",
-    0,
-];
-
-const catNames: string[] = [catData.filter(name => name === "string")];
-const catAges: number[] = [];
->>>>>>> 0afc4cbb21a80fdd624b7d856aee18d6240dc565
 
 
 console.log("Names: ", catNames);
 console.log("Ages: ", catAges);
 
 document.body.innerHTML = `
-<<<<<<< HEAD
     <ul>
       ${catNames
         .map((name, index) => `<li>${name} - ${catAges[index]}</li>`)
@@ -49,11 +29,3 @@ document.body.innerHTML = `
   `;
 console.log();
 
-=======
-                              <ul>
-                                  ${catNames
-        .map((name, index) => `<li>${name} - ${catAges[index]}</li>`)
-        .join("")}
-                                                </ul>`
-                                  
->>>>>>> 0afc4cbb21a80fdd624b7d856aee18d6240dc565
